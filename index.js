ce6a6ef9d8df5413c68359f2f18f595c86fbc432
xpatch <jestersi@gmail.com>
2013-11-15 06:17:50 +0000

    notes on templates

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
+/*
 
+    ECT fast template with coffee script
+    
+    
+http://coffeescript.org/
+http://ectjs.com/
+https://github.com/visionmedia/consolidate.js
+
+https://github.com/RandomEtc/ejs-locals
+https://github.com/visionmedia/ejs/tree/master/examples
+
+
+https://github.com/chovy/express-template-demo
+
+http://stackoverflow.com/questions/12209607/using-layout-view-in-express-with-consolidate-and-mustache
+
+
+
+
+
+*/
 
 
 var express = require('express');
 var request = require('request');
 var engines = require('consolidate');
-
 
 
 
