854abe435802e4008c377ffbdb915e13b0a1fff4
xpatch <jestersi@gmail.com>
2013-11-16 06:51:09 +0000

    asdkfshifoiwhfeoihew

---
+++ index.working.js
@@ -0,0 +1,73 @@
+/*
+
+    ECT fast template with coffee script
+    
+    Hogan.js - Twitter template engine.
+    
+    http://expressjs.com/api.html#app.engine    
+    
+    
+    
+http://coffeescript.org/
+http://ectjs.com/
+https://github.com/visionmedia/consolidate.js
+
+https://github.com/RandomEtc/ejs-locals
+https://github.com/visionmedia/ejs/tree/master/examples
+
+
+https://github.com/chovy/express-template-demo
+
+http://stackoverflow.com/questions/12209607/using-layout-view-in-express-with-consolidate-and-mustache
+
+
+*/
+
+
+var express = require('express');
+var request = require('request');
+
+//var cons = require('consolidate');
+//console.log(cons);
+
+var app = express();
+
+
+var ECT = require('ect');
+
+
+var ectRenderer = ECT({ 
+  watch: true, 
+  cache:false,
+  //root: __dirname + '/views',
+  open: '{{',
+  close: '}}'
+});
+
+app.engine('.ect', ectRenderer.render);
+
+
+app.set('view engine', 'ect');
+app.set('views', __dirname + '/views');
+
+
+app.locals = {
+    title: '&copy;0d3 5n1p3&reg;'
+};
+
+
+app.get('/', function (req,res) {
+    res.render('index.ect',{title:'punk monkeyness'});
+});
+
+
+/*
+app.get( '/', function(req,res,next){
+    res.send('Alo?');
+});
+*/
+
+
+
+
+app.listen(process.env.PORT);
